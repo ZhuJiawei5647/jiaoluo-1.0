9180c7ce470bab3252d0cf5995ae56d94ba70546
zhujiawei <492997027@qq.com>
2018-07-06 17:03:41 +0800

    706

--- js/util.js
+++ js/util.js
@@ -9,6 +9,26 @@ var util = {
         r = null;
         return context == null || context == "" || context == "undefined" ? "" : context;
 	},
+    dateFormat: (function () {
+        Date.prototype.Format = function (fmt) { //author: meizz 
+            var o = {
+                "M+": this.getMonth() + 1, //月份 
+                "d+": this.getDate(), //日 
+                "h+": this.getHours(), //小时 
+                "m+": this.getMinutes(), //分 
+                "s+": this.getSeconds(), //秒 
+                "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+                "S": this.getMilliseconds() //毫秒 
+            };
+            if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+            for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            return fmt;
+        }
+        return function (time, format) {
+            return new Date(time).Format(format)
+        }
+    })(),
     clacImgZoomParam:function (maxWidth, maxHeight, width, height) {
         var param = {
             top: 0,
